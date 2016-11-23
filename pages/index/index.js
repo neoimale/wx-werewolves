a74a28a044f24bdb8ef3cd229b64cacd61f13883
Neo Shan <neoimale@gmail.com>
2016-11-23 15:52:48 +0800

    home page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,5 @@
-//index.js
-//获取应用实例
-var app = getApp()
+//index2.js
+
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../index2/index2'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    data: {}
 })
