7bba19e7acb5bcee0bac19e78342c3a934265ca6
Tommy <humingtao@taojinjia.com>
2016-11-29 18:05:51 +0800

    单选改多选...

--- pages/create-room/create-room.js
+++ pages/create-room/create-room.js
@@ -36,8 +36,8 @@ Page({
       })
   },
 
-  radioChange: function(e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  checkboxChange: function(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
 
   clickCreateGameAction: function(e) {
