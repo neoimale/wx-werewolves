71f39e7d36814c856c5dc7cd250912b4f736235f
Neo Shan <neoimale@gmail.com>
2016-12-07 15:51:08 +0800

    something about style

--- utils/const.js
+++ utils/const.js
@@ -1,7 +1,14 @@
 const HTTPS_URL = 'https://api.byutech.cn'; 
 const WSS_URL = 'wss://api.byutech.cn';
 
+const GAME_WOLF = 100;
+const GAME_KILLER = 101;
+
 module.exports = {
     HTTPS_URL: HTTPS_URL,
-    WSS_URL: WSS_URL
+    WSS_URL: WSS_URL,
+    GAME: {
+        GAME_WOLF: GAME_WOLF,
+        GAME_KILLER: GAME_KILLER
+    }
 }
