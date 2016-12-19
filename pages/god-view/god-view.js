56a003a615cb2b184bdc6c848997fc2f887b7d6a
Neo Shan <neoimale@gmail.com>
2016-12-19 16:30:50 +0800

    god view done

--- pages/god-view/god-view.js
+++ pages/god-view/god-view.js
@@ -37,16 +37,57 @@ Page({
           })
           break;
         }
+        case 'dead': {
+          let id = msg.content.message;
+          let players = this.data.players;
+          let match = _.find(players, item => item.id == id);
+          if(match) {
+            match.status = 'dead';
+          }
+          this.setData({
+            players: players
+          })
+          break;
+        }
+        case 'bringback': {
+          let id = msg.content.message;
+          let players = this.data.players;
+          let match = _.find(players, item => item.id == id);
+          if(match) {
+            match.status = '';
+          }
+          this.setData({
+            players: players
+          })
+          break;
+        }
+        case 'headset': {
+          let newId = msg.content.message.newId;
+          let oldId = msg.content.message.oldId;
+          let players = this.data.players;
+          _.each(players, (item) => {
+            if(item.id == newId) {
+              item.isHead = true;
+            } else {
+              item.isHead = false;
+            }
+          })
+          this.setData({
+            players: players
+          })
+          break;
+        }
       }
     } catch(e) {
       console.error(e);
     }
   },
   processPlayerInfo: function(player) {
-    let info = JSON.parse(player.info);
+    let info = player.info;
     let roleName = Util.translateRole(this.data.roomType, player.role);
     let color = Util.roleColor(player.role);
     return {
+      id: player.id,
       num: player.num,
       color: color,
       nickName: info.nickName,
@@ -71,6 +112,7 @@ Page({
           url: "wss://api.byutech.cn/ws/" + res.data + "?room=" + roomNumber +"&business=god"
         })
         wx.onSocketOpen(() => this.socketOpen = true);
+        wx.onSocketClose(() => this.socketOpen = false);
         wx.onSocketMessage((msg) => {
           this.onMessage(msg.data);
         })
@@ -79,9 +121,61 @@ Page({
   },
   onTapPlayer: function(e) {
     console.log(e);
-    wx.showActionSheet({
-      itemList: ['选为警长', '死亡']
+    let actions = [];
+    let messages = [];
+    let player = _.find(this.data.players, (item) => {
+      return item.id == e.currentTarget.id;
     })
+    if(player) {
+      if(!player.head) {
+        actions.push('选为警长');
+        messages.push({
+          type: 1,
+          content: {
+            event: 'head',
+            message: {
+              room: this.data.roomNum,
+              key: player.id
+            }
+          }
+        })
+      }
+      if(player.status == 'dead') {
+        actions.push('撤销死亡');
+        messages.push({
+          type: 1,
+          content: {
+            event: 'reborn',
+            message: {
+              room: this.data.roomNum,
+              key: player.id
+            }
+          }
+        })
+      } else {
+        actions.push('死亡');
+        messages.push({
+          type: 1,
+          content: {
+            event: 'death',
+            message: {
+              room: this.data.roomNum,
+              key: player.id
+            }
+          }
+        })
+      }
+      wx.showActionSheet({
+        itemList: actions,
+        success: (res) => {
+          if(!res.cancel && this.socketOpen) {
+            wx.sendSocketMessage({
+              data: JSON.stringify(messages[res.tapIndex])
+            })
+          }
+        }
+      })
+    }
   },
   restartGame: function() {
     wx.showModal({
@@ -101,15 +195,6 @@ Page({
         }
       }
     })
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
   },
   onUnload:function(){
     // 页面关闭
